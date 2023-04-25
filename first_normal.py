13fb4e2806ea92049cdc0cc34fe791dbe19ced73
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-25 12:47:09 -0700

    Create first_normal.py

---
+++ first_normal.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+# Connect to the SQLite3 database
+conn = sqlite3.connect('pokemon.sqlite')
+c = conn.cursor()
+c.execute("DROP TABLE IF EXISTS abilities_new")
+
+
+# Create a new table to store the split values
+c.execute('''CREATE TABLE abilities_new
+             (ability TEXT, num TEXT)''')
+
+c.execute("SELECT abilities, pokedex_number FROM imported_pokemon_data")
+rows = c.fetchall()
+for row in rows:
+    ability = row[0].split(',')
+    number = row[1]
+    for t in ability:
+        c.execute("INSERT INTO abilities_new (ability, num) VALUES (?,?)", (t, number))
+        
+conn.commit()
+c.close()
