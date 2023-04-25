e1c20e980e492c0ec0593b4cd90bed096956ea4f
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-25 14:59:30 -0700

    Update second_normal.sql

--- second_normal.sql
+++ second_normal.sql
@@ -1,19 +1,23 @@
+-- Creating table for abilities in order to remove partial dependency
 CREATE TABLE Abilities (
   pokedex_number INT,
   ability TEXT,
   FOREIGN KEY (pokedex_number) REFERENCES normalized(pokedex_number)
 );
 
+-- Moving the ability column over while keeping pokedex_number as the primary key
 INSERT INTO Abilities (pokedex_number, ability)
 SELECT pokedex_number, ability
 FROM normalized;
 
+-- Creating table for Type in order to remove partial dependency
 CREATE TABLE Type (
   pokedex_number INT,
   type1 TEXT,type2 TEXT,                      
   FOREIGN KEY (pokedex_number) REFERENCES normalized(pokedex_number)
 );
 
+-- Moving the type1 and type2 column over while keeping pokedex_number as the primary key
 INSERT INTO Type (pokedex_number, type1, type2)
 SELECT pokedex_number ,type1, type2
 FROM normalized;
