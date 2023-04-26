8e6258d03bc62045905c25476d9f485786e52593
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-25 23:26:28 -0700

    Update third_normal.sql

--- third_normal.sql
+++ third_normal.sql
@@ -1,4 +1,4 @@
-
+-- Created a seperate stats table to eliminate transitive dependicies
 Create table Stats(pokedex_number INT,"hp" TEXT,"attack" TEXT,"defense" TEXT,"sp_attack" TEXT,
   "sp_defense" TEXT,"speed" TEXT,"generation" TEXT);
 
@@ -8,13 +8,14 @@ SELECT pokedex_number ,"hp" ,"attack","defense","sp_attack",
   "sp_defense","speed",generation
 FROM first_normalized;
 
+-- Created a seperate general info table to eliminate transitive dependicies
 create table General(pokedex_number INT,"name" TEXT,"is_legendary");
 
 INSERT INTO General(pokedex_number,name,is_legendary)                                                      
 SELECT pokedex_number,name,is_legendary
 FROM first_normalized; 
 
-
+--Dropped the columns that were moved to seperate tables
 ALTER TABLE first_normalized  DROP attack;
 ALTER TABLE first_normalized  DROP defense;
 ALTER TABLE first_normalized  DROP hp;
